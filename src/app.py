843d6bc22a5bc6bfbb7a0e629593a1f8352e4288
user <user@users-MBP.hsd1.ga.comcast.net>
2022-09-16 23:04:49 -0400

    more changes

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
 # main program
-from src.s_class.s_usb import dev_connect
+from src.s_class.s_usb import dev_find, dev_command
 
+
+dev = []
+num_of_dev = 0
 
 def run():
-	d = dev_connect()
-	print(d.find_all_devices())
-	dev = input("Enter device to connect to: ")
-	d.open_device(dev)
-	print(d.find_device_data())
+	d = dev_find()
+	ld = d.find_all_devices()
+	print(ld)
+	for i in ld:
+		if "USB" in i:
+			num_of_dev += 1
+	if num_of_dev == 2:
+		sig_gen_code = input("Enter device code to connect to Signal Generator: ")
+		os_code = input("Enter device code to connect to Oscilloscope: ")
+		dev.append(dev_command(sig_gen_code))
+		dev.append(dev_command(os_code))
+	elif num_of_dev == 1:
+		sig_gen_code = input("Enter device code to connect to Signal Generator: ")
+		dev.append(dev_command(sig_gen_code))
--- src/s_class/s_usb.py
+++ src/s_class/s_usb.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 import numpy
 
 
-class dev_connect:
+class dev_find:
     def __init__(self):
-        self.rm = pyvisa.ResourceManager('@py')
+        self.rm = pyvisa.ResourceManager()
 
 
     def find_all_devices(self):
@@ -13,18 +13,15 @@ class dev_connect:
         return ld
 
 
-    def find_device_data(self):
-        dd = self.dev.query("*IDN?")
-        return dd
-
-
-    def open_device(self, dev):
-        self.dev = self.rm.open_resource(dev)
-
+class dev_command:
+    def __init__(self, dev):
+        rm = pyvisa.ResourceManager()
+        self.dev = rm.open_resource(dev)
+        self.dd = self.dev.query("*IDN?")
+        print(f"Connecting to {self.dd}")
 
     def send_data(self, d):
         pass
-
 
     def recv_data(self):
         pass
