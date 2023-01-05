83736ecb58d82a613f104fc38376fff678fccfdb
msle237-lees <msle237.lees@gmail.com>
2023-01-05 10:32:07 -0500

    changed to macbook for easy transport, my dell is too heavy sometimes

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-# TO-DO LIST:
-#    Oscilloscope Channels had a background color of yellow for 1 and light blue for 2.
-#    Signal Generator Channels had a background of red for 1 and blue for 2.
-#    Make the Enable checkbox and font bigger.
-#    Confirm buttons save the settings to an array for writing to the device.
+# TO-DO List
+# 1. Using the images that glen sent, create the necessary commands for later
+# 2. On each confirm button press, the commands should be sent to their cooresponding machines
+# 3. Upload to github for glen to test
+# 4. Fix the errors that will pop up
 
-import PySimpleGUI as sg
 import os
 import sys
+
 import numpy as np
 import pandas as pd
+import PySimpleGUI as sg
 import pyvisa as visa
 
-
+# define the resource manager
+global rm
+rm = visa.ResourceManager()
 
 class hid_device:
     def __init__(self):
@@ -29,14 +32,18 @@ class hid_device:
         self.dev = self.rm.open_resource(self.HWID, write_termination = '\n', read_termination='\n')
         print(f"Connecting to {self.dev}")
     
-    def Send_command(self, command):
+    def Write_Command(self, command):
         self.dev.write(command)
         return
 
     def Read_command(self):
         x = self.dev.read()
-        print(x)
         return x
+
+    def Query_Command(self, command):
+        x = self.dev.query(command) 
+        return(x)
+
 
 def calculate_Frequency(freq_min, freq_min_unit, freq_max, freq_max_unit, freq_step, freq_step_unit):
     freq_list = []
@@ -71,8 +78,6 @@ def calculate_Frequency(freq_min, freq_min_unit, freq_max, freq_max_unit, freq_s
     return freq_list
 
 def main():
-    rm = visa.ResourceManager()
-
     sg.theme('Dark')   # Add a touch of color
 
     # All the stuff inside your window.
@@ -81,6 +86,7 @@ def main():
                                     [sg.Text('Volts:', size=(15, 1)), sg.InputText(key='-OSC_CH1_VOLTS_IN-'), sg.DropDown(['V', 'mV', 'uV'], size=(5, 1), key='-OSC_CH1_VOLTS_UNIT-')],
                                     [sg.Text('Horizontal Time Division: ', size=(15, 1)), sg.InputText(key='-OSC_CH1_HORZ_TIME_DIV_IN-'), sg.DropDown(['s', 'ms', 'us', 'ns'], size=(5, 1), key='-OSC_CH1_HORZ_TIME_DIV_UNIT-')],
                                     [sg.Text('Measure: ', size=(15, 1)), sg.Checkbox('Vpp', default=False, size=(15,1), key='-OSC_CH1_MEAS_VPP-'), sg.Checkbox('Phase', default=False, size=(15,1), key='-OSC_CH1_MEAS_PHASE-'), sg.Checkbox('Frequency', default=False, size=(15,1), key='-OSC_CH1_MEAS_FREQ-')],
+                                    [sg.Text('Acquistion Mode:', size=(15, 1)), sg.DropDown(['Normal', 'Averages', 'Peak', 'Hresolution'], size=(5, 1), key='-OSC_CH1_ACQ_MODE-')],
                                     [sg.Button('Confirm', key='-OSC_CH1_CONFIRM-')]
                                 ]
     Oscilloscope_Channel_2_Tab = [  [sg.Text('Oscilloscope Channel 2 Settings', size=(30, 1), font=("Helvetica", 20)), sg.Graph(canvas_size=(30, 30), graph_bottom_left=(0, 0), graph_top_right=(30, 30), background_color='lightblue', key='-OSC_CH2_GRAPH-')],
@@ -88,6 +94,7 @@ def main():
                                     [sg.Text('Volts:', size=(15, 1)), sg.InputText(key='-OSC_CH2_VOLTS_IN-'), sg.DropDown(['V', 'mV', 'uV'], size=(5, 1), key='-OSC_CH2_VOLTS_UNIT-')],
                                     [sg.Text('Horizontal Time Division: ', size=(15, 1)), sg.InputText(key='-OSC_CH2_HORZ_TIME_DIV_IN-'), sg.DropDown(['s', 'ms', 'us', 'ns'], size=(5, 1), key='-OSC_CH2_HORZ_TIME_DIV_UNIT-')],
                                     [sg.Text('Measure: ', size=(15, 1)), sg.Checkbox('Vpp', default=False, size=(15,1), key='-OSC_CH2_MEAS_VPP-'), sg.Checkbox('Phase', default=False, size=(15,1), key='-OSC_CH2_MEAS_PHASE-'), sg.Checkbox('Frequency', default=False, size=(15,1), key='-OSC_CH2_MEAS_FREQ-')],
+                                    [sg.Text('Acquistion Mode:', size=(15, 1)), sg.DropDown(['Normal', 'Averages', 'Peak', 'Hresolution'], size=(5, 1), key='-OSC_CH2_ACQ_MODE-')],
                                     [sg.Button('Confirm', key='-OSC_CH2_CONFIRM-')]
                                 ]
 
@@ -97,7 +104,7 @@ def main():
                         ]
 
     Signal_Generator_Channel_1_Tab = [  [sg.Text('Signal Generator Channel 1 Settings', size=(30, 1), font=("Helvetica", 20)), sg.Graph(canvas_size=(30, 30), graph_bottom_left=(0, 0), graph_top_right=(30, 30), background_color='red', key='-SG_CH1_GRAPH-')],
-                                        [sg.Checkbox('Enable', default=False, size=(25,1), font=("Helvetica", 20), key='SG_CHAN_1_ENABLE')],
+                                        [sg.Checkbox('Enable', default=False, size=(25,1), font=("Helvetica", 20), key='-SG_CHAN_1_ENABLE-')],
                                         [sg.Text('Signal Type: '), sg.DropDown(['Sine', 'Square', 'Triangle', 'Ramp', 'Pulse', 'Noise', 'DC'], size=(10, 1), key='-SG_CHAN_1_SIGNAL_TYPE-')],
                                         [sg.Text('Frequency Min: '), sg.InputText(key='-SG_CHAN_1_FREQ_MIN_IN-'), sg.DropDown(['Hz', 'kHz', 'MHz', 'GHz'], size=(10, 1), key='-SG_CHAN_1_FREQ_MIN_UNIT-')],
                                         [sg.Text('Frequency Max: '), sg.InputText(key='-SG_CHAN_1_FREQ_MAX_IN-'), sg.DropDown(['Hz', 'kHz', 'MHz', 'GHz'], size=(10, 1), key='-SG_CHAN_1_FREQ_MAX_UNIT-')],
@@ -140,100 +147,157 @@ def main():
             ]
 
     # Create the Window
-    window = sg.Window('Window Title', layout, size=(1280, 720), resizable=True, finalize=True)
+    window = sg.Window('Window Title', layout, size=(1280, 720), resizable=True)
 
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
         if event == '-OSC_CH1_CONFIRM-':
-            OSC_CHANN_1_SETT = [values['-OSC_ID-'],
-                                values['-OSC_CH1_ENABLE-'],
-                                values['-OSC_CH1_VOLTS_IN-'],
-                                values['-OSC_CH1_VOLTS_UNIT-'],
-                                values['-OSC_CH1_HORZ_TIME_DIV_IN-'],
-                                values['-OSC_CH1_HORZ_TIME_DIV_UNIT-'],
-                                values['-OSC_CH1_MEAS_VPP-'],
-                                values['-OSC_CH1_MEAS_PHASE-'],
-                                values['-OSC_CH1_MEAS_FREQ-']]
-            print(OSC_CHANN_1_SETT)
+            OSC_CHANN_1_SETT = [values['-OSC_ID-'],                                 # 0
+                                values['-OSC_CH1_ENABLE-'],                         # 1
+                                values['-OSC_CH1_VOLTS_IN-'],                       # 2
+                                values['-OSC_CH1_VOLTS_UNIT-'],                     # 3
+                                values['-OSC_CH1_HORZ_TIME_DIV_IN-'],               # 4
+                                values['-OSC_CH1_HORZ_TIME_DIV_UNIT-'],             # 5
+                                values['-OSC_CH1_MEAS_VPP-'],                       # 6
+                                values['-OSC_CH1_MEAS_PHASE-'],                     # 7
+                                values['-OSC_CH1_MEAS_FREQ-'],                      # 8
+                                values['-OSC_CH1_ACQ_MODE-']]                       # 9
 
             # Bind the hid_device class object to the oscilloscope ID address
             oscilloscope_object = hid_device()
             oscilloscope_object.Connect(OSC_CHANN_1_SETT[0])
 
-            # Add the send command function here with proper arguments to send the settings to the oscilloscope for channel 1
-
-
         elif event == '-OSC_CH2_CONFIRM-':
-            OSC_CHANN_2_SETT = [values['-OSC_ID-'],
-                                values['-OSC_CH2_ENABLE-'],
-                                values['-OSC_CH2_VOLTS_IN-'],
-                                values['-OSC_CH2_VOLTS_UNIT-'],
-                                values['-OSC_CH2_HORZ_TIME_DIV_IN-'],
-                                values['-OSC_CH2_HORZ_TIME_DIV_UNIT-'],
-                                values['-OSC_CH2_MEAS_VPP-'],
-                                values['-OSC_CH2_MEAS_PHASE-'],
-                                values['-OSC_CH2_MEAS_FREQ-']]
-            print(OSC_CHANN_2_SETT)
-
-            # Add the send command function here with proper arguments to send the settings to the oscilloscope for channel 2
+            OSC_CHANN_2_SETT = [values['-OSC_ID-'],                                 # 0
+                                values['-OSC_CH2_ENABLE-'],                         # 1
+                                values['-OSC_CH2_VOLTS_IN-'],                       # 2
+                                values['-OSC_CH2_VOLTS_UNIT-'],                     # 3
+                                values['-OSC_CH2_HORZ_TIME_DIV_IN-'],               # 4
+                                values['-OSC_CH2_HORZ_TIME_DIV_UNIT-'],             # 5
+                                values['-OSC_CH2_MEAS_VPP-'],                       # 6
+                                values['-OSC_CH2_MEAS_PHASE-'],                     # 7
+                                values['-OSC_CH2_MEAS_FREQ-'],                      # 8
+                                values['-OSC_CH2_ACQ_MODE-']]                       # 9
 
         elif event == '-SG_CHAN_1_CONFIRM-':
-            SG_CHANN_1_SETT = [values['-SG_ID-'],
-                                values['-SG_CHAN_1_FREQ_MIN_IN-'],
-                                values['-SG_CHAN_1_FREQ_MIN_UNIT-'],
-                                values['-SG_CHAN_1_FREQ_MAX_IN-'],
-                                values['-SG_CHAN_1_FREQ_MAX_UNIT-'],
-                                values['-SG_CHAN_1_FREQ_STEP_IN-'],
-                                values['-SG_CHAN_1_FREQ_STEP_UNIT-'],
-                                values['-SG_CHAN_1_AMPLITUDE_IN-'],
-                                values['-SG_CHAN_1_AMPLITUDE_UNIT-'],
-                                values['-SG_CHAN_1_OFFSET_IN-'],
-                                values['-SG_CHAN_1_OFFSET_UNIT-']]
-            print(SG_CHANN_1_SETT)
+            SG_CHANN_1_SETT = [values['-SG_ID-'],                                   # 0
+                                values['-SG_CHAN_1_ENABLE-'],                       # 1
+                                values['-SG_CHAN_1_FREQ_MIN_IN-'],                  # 2
+                                values['-SG_CHAN_1_FREQ_MIN_UNIT-'],                # 3
+                                values['-SG_CHAN_1_FREQ_MAX_IN-'],                  # 4
+                                values['-SG_CHAN_1_FREQ_MAX_UNIT-'],                # 5
+                                values['-SG_CHAN_1_FREQ_STEP_IN-'],                 # 6
+                                values['-SG_CHAN_1_FREQ_STEP_UNIT-'],               # 7
+                                values['-SG_CHAN_1_AMPLITUDE_IN-'],                 # 8
+                                values['-SG_CHAN_1_AMPLITUDE_UNIT-'],               # 9
+                                values['-SG_CHAN_1_OFFSET_IN-'],                    # 10
+                                values['-SG_CHAN_1_OFFSET_UNIT-'],                  # 11
+                                values['-SG_CHAN_1_SIGNAL_TYPE-']]                  # 12
 
-            # Bind the hid_device class object to the signal generator ID address
-            # Add the send command function here with proper arguments to send the settings to the signal generator for channel 1
+            # Create the signal generator object and bind it to the signal generator ID address
+            signal_generator_object = hid_device()
+            signal_generator_object.Connect(SG_CHANN_1_SETT[0])
+
             # Add the calculate frequency function here with proper arguments to calculate the frequency for channel 1
             SG_CHANN_1_FREQ = calculate_Frequency(SG_CHANN_1_SETT[1], SG_CHANN_1_SETT[2], SG_CHANN_1_SETT[3], SG_CHANN_1_SETT[4], SG_CHANN_1_SETT[5], SG_CHANN_1_SETT[6])
-            print(SG_CHANN_1_FREQ)
 
         elif event == '-SG_CHAN_2_CONFIRM-':
-            SG_CHANN_2_SETT = [values['-SG_ID-'],
-                                values['-SG_CHAN_2_FREQ_MIN_IN-'],
-                                values['-SG_CHAN_2_FREQ_MIN_UNIT-'],
-                                values['-SG_CHAN_2_FREQ_MAX_IN-'],
-                                values['-SG_CHAN_2_FREQ_MAX_UNIT-'],
-                                values['-SG_CHAN_2_FREQ_STEP_IN-'],
-                                values['-SG_CHAN_2_FREQ_STEP_UNIT-'],
-                                values['-SG_CHAN_2_AMPLITUDE_IN-'],
-                                values['-SG_CHAN_2_AMPLITUDE_UNIT-'],
-                                values['-SG_CHAN_2_OFFSET_IN-'],
-                                values['-SG_CHAN_2_OFFSET_UNIT-']]
-            print(SG_CHANN_2_SETT)
+            SG_CHANN_2_SETT = [values['-SG_ID-'],                                   # 0
+                                values['-SG_CHAN_1_ENABLE-'],                       # 1
+                                values['-SG_CHAN_2_FREQ_MIN_IN-'],                  # 2
+                                values['-SG_CHAN_2_FREQ_MIN_UNIT-'],                # 3
+                                values['-SG_CHAN_2_FREQ_MAX_IN-'],                  # 4
+                                values['-SG_CHAN_2_FREQ_MAX_UNIT-'],                # 5
+                                values['-SG_CHAN_2_FREQ_STEP_IN-'],                 # 6
+                                values['-SG_CHAN_2_FREQ_STEP_UNIT-'],               # 7
+                                values['-SG_CHAN_2_AMPLITUDE_IN-'],                 # 8
+                                values['-SG_CHAN_2_AMPLITUDE_UNIT-'],               # 9
+                                values['-SG_CHAN_2_OFFSET_IN-'],                    # 10
+                                values['-SG_CHAN_2_OFFSET_UNIT-'],                  # 11
+                                values['-SG_CHAN_2_SIGNAL_TYPE-']]                  # 12
 
-            # Add the send command function here with proper arguments to send the settings to the signal generator for channel 2
             # Add the calculate frequency function here with proper arguments to calculate the frequency for channel 2
             SG_CHANN_2_FREQ = calculate_Frequency(SG_CHANN_2_SETT[1], SG_CHANN_2_SETT[2], SG_CHANN_2_SETT[3], SG_CHANN_2_SETT[4], SG_CHANN_2_SETT[5], SG_CHANN_2_SETT[6])
-            print(SG_CHANN_2_FREQ)
 
         elif event == '-OUTPUT_CONFIRM-':
-            OUTPUT_SETT = [ values['-OUTPUT_FILE_NAME-'],
-                            values['-OUTPUT_FILE_FORMAT-'],
-                            values['-OUTPUT_FILE_LOCATION-'] ]
-            print(OUTPUT_SETT)
-
-            # Add the send command function here with proper arguments to send the settings to the output settings
+            OUTPUT_SETT = [ values['-OUTPUT_FILE_NAME-'],                           # 0
+                            values['-OUTPUT_FILE_FORMAT-'],                         # 1
+                            values['-OUTPUT_FILE_LOCATION-'] ]                      # 2
 
         elif event == '-EXECUTE-':
-            print('Execute')
-
             # Add the send command function here with proper arguments to send the settings to the signal generator and oscilloscope to start the measurement
+            # Configure the Oscilloscope first
+            # 0.1 Enable Channel 1 and 2 dependent on user input
+            if OSC_CHANN_1_SETT[1]:
+                # Add the send command function here with proper arguments to enable Channel 1
+                oscilloscope_object.Send_Command('CHAN1:DISP ON')
+
+            if OSC_CHANN_2_SETT[1]:
+                # Add the send command function here with proper arguments to enable Channel 2
+                pass
+        
+            # 0.2 Configure Oscilloscope parameters for each channel
+            if OSC_CHANN_1_SETT[1]:
+                # Add the send command function here with proper arguments to configure Channel 1
+                pass
+            if OSC_CHANN_2_SETT[1]:
+                # Add the send command function here with proper arguments to configure Channel 2
+                pass
+
+            # Configure the Signal Generator second
+            # 0.1 Enable Channel 1 and 2 dependent on user input
+            if SG_CHANN_1_SETT[1]:
+                # Add the send command function here with proper arguments to enable Channel 1
+                pass
+            if SG_CHANN_2_SETT[1]:
+                # Add the send command function here with proper arguments to enable Channel 2
+                pass
+
+            # 0.2 Configure the Signal Generator parameters for each channel
+            if SG_CHANN_1_SETT[1]:
+                # Add the send command function here with proper arguments to configure Channel 1
+                pass
+            if SG_CHANN_2_SETT[1]:
+                # Add the send command function here with proper arguments to configure Channel 2
+                pass
+
+            # Create a 2D array with two inner arrays
+            # The first inner array will store the data from Channel 1
+            # The second inner array will store the data from Channel 2
+            temp = [[],[]]
+
+            # for loop in range(0, SG_CHANN_1_FREQ)
+            for i in range(0, SG_CHANN_1_FREQ):
+                # Instruct the Signal Generator to send the wavelength on Channel 1
+                if SG_CHANN_1_SETT[1]:
+                    # Add the send command function here with proper arguments to send the wavelength to Channel 1
+                    pass
+
+                # Instruct the Oscilloscope to read the data and store in first array of temp 2D array
+                if OSC_CHANN_1_SETT[1]:
+                    # Add the send command function here with proper arguments to read the data from Channel 1
+                    pass
+            
+            # for loop in range(0, SG_CHANN_2_FREQ)
+            for i in range(0, SG_CHANN_2_FREQ):
+                # Instruct the Signal Generator to send the wavelength on Channel 2
+                if SG_CHANN_2_SETT[1]:
+                    # Add the send command function here with proper arguments to send the wavelength to Channel 2
+                    pass
+                # Instruct the Oscilloscope to read the data and store in second array of temp 2D array
+                if OSC_CHANN_2_SETT[1]:
+                    # Add the send command function here with proper arguments to read the data from Channel 2
+                    pass
+
+            # Compile the results into a single 2D array and update the results table in the results window
+            # Create the bode plot and update the results window
+
+            pass
 
         elif event == '-STOP-':
-            print('Stop')
-
             # Add the send command function here with proper arguments to send the settings to the signal generator and oscilloscope to stop the measurement
+            pass
 
         elif event == sg.WIN_CLOSED: # if user closes window
             break
